789aa1f035b3fce093d4e9254d23b062f2f29eed
Sedat Demirkol <sdemirkol@ceobee.dev>
2023-10-03 14:31:46 +0300

    updated

--- components/BlogCard/index.js
+++ components/BlogCard/index.js
@@ -5,20 +5,19 @@ import styles from './styles.module.css'
 
 function BlogCard({ title, body, id }) {
     return <Link href={`/${id}`} className={styles.card}>
-            <div className={styles.cardImage}>
-                {/* Cumulative Layout Shift - Kümülatif Düzen Kaymasını engellemek için next.js bizden width ve height bilgilerini istiyor. */}
-                <Image
-                    src={`https://picsum.photos/200/300?random=${id}`}
-                    alt='Blog Card Image'
-                    fill
-                // width={96}
-                // height={96}
-                />
-            </div>
-            <div className={styles.cardBody}>
-                <h3>{title}</h3>
-                <p>{body}</p>
-            </div>
+        <div className={styles.cardImage}>
+            {/* Cumulative Layout Shift - Kümülatif Düzen Kaymasını engellemek için next.js bizden width ve height bilgilerini istiyor. */}
+            <Image
+                src={`https://picsum.photos/200/200?random=${id}`}
+                alt='Blog Card Image'
+                width={96}
+                height={96}
+            />
+        </div>
+        <div className={styles.cardBody}>
+            <h3>{title}</h3>
+            <p>{body}</p>
+        </div>
     </Link>
 }
 
